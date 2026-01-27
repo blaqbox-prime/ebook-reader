e04f35096c0c8f8f891be4d0df364118a2b7a21e
Karabo Sambo <karabosambo@outlook.com>
2026-01-27 13:32:19 +0200

    created library view and metadata extraction from epubs

---
+++ src/components/BookTile.tsx
@@ -0,0 +1,54 @@
+import { images } from "@/assets";
+import { Book } from "@/src/data/watermelondb/models";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Image, Text, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
+import { PressableScale } from 'pressto';
+
+type BookTileProps = {
+  book: Book;
+};
+
+
+
+const BookTile = ({ book }: BookTileProps) => {
+
+  const [cover, _] = useState(book.coverImage);
+
+
+  return (
+    <Link
+      href={{
+        pathname: "/",
+        // params: { uri: book.uri },
+      }}
+      asChild
+      key={book.uri}
+    >
+      <TouchableOpacity onPress={() => {console.log("Pressto")}}>
+        <View className="mb-4 w-full p-1">
+          <Image
+            source={
+              cover
+                ? { uri: cover as string }
+                : images.cover
+            }
+            resizeMode="cover"
+            className="h-[270px] w-full rounded-xl "
+          />
+
+          <View className="mt-6 px-4">
+            <Text className="font-lato-black line-clamp-2 leading-snug tracking-wide">
+              {book.title}
+            </Text>
+            <Text className="text-sm line-clamp-2 text-gray-600 leading-relaxed tracking-wide mt-2">
+              {book.author}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  );
+};
+
+export default BookTile;
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,22 @@
+import Feather from '@expo/vector-icons/Feather'
+import { TextInput, View } from 'react-native'
+
+type SearchBoxProps = {
+    onChangeText: (text :string) => void,
+    className?: string | undefined,
+}
+
+const SearchBox = ({onChangeText, className}: SearchBoxProps) => {
+  return (
+    <View className={`bg-primary-900 w-full py-2 px-4 rounded-full flex-row gap-3 items-center ${className}`}>
+                    <Feather name="search" size={22} color="black" />
+                    <TextInput
+                      placeholder="Search book title..."
+                      className="text-black"
+                      onChangeText={(text) => onChangeText(text)}
+                    />
+                  </View>
+  )
+}
+
+export default SearchBox
